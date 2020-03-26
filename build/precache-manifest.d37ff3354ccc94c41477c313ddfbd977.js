ded389ba37e7295444b77486de48376e988a7ae6
KryptKode <paulakhamiogu@gmail.com>
2020-03-26 13:50:42 +0100

    Add xeen prod

---
+++ build/precache-manifest.d37ff3354ccc94c41477c313ddfbd977.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1977b9880a77c72655d423596e797075",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47455d90a4633c258d23",
+    "url": "/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "17061d698f7314ec21ab",
+    "url": "/static/js/2.f0e72e9a.chunk.js"
+  },
+  {
+    "revision": "6772af2796175178b64d65a6c449a709",
+    "url": "/static/js/2.f0e72e9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47455d90a4633c258d23",
+    "url": "/static/js/main.2a5ce6d9.chunk.js"
+  },
+  {
+    "revision": "6617d3774db682339d45",
+    "url": "/static/js/runtime-main.0e42e885.js"
+  },
+  {
+    "revision": "94b6b5e339b4bfd61c3f550dc1459ae0",
+    "url": "/static/media/back.94b6b5e3.svg"
+  },
+  {
+    "revision": "9a4807395a42d7aa89fa20aedc9af448",
+    "url": "/static/media/logo.9a480739.svg"
+  },
+  {
+    "revision": "d0404e7b378c418160da8b864b2b6d67",
+    "url": "/static/media/logout.d0404e7b.svg"
+  },
+  {
+    "revision": "6ec4fce45c9a37cee9c1faa175fe8f69",
+    "url": "/static/media/server.6ec4fce4.svg"
+  }
+]);
